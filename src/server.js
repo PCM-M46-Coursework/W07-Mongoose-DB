73ce8a1947c1b46fa158bdb1279f652e26fcc5c7
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-13 17:33:38 +0100

    feat(routes): add GET single book route

--- src/server.js
+++ src/server.js
@@ -195,6 +195,31 @@ app.get('/books', async (req, res) =>
     }
 });
 
+/**
+ * Get a specific book, based on the SEO slug.
+ * 
+ * @name GET /book/:isbn
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @returns {object} Returns a JSON object with the requested book.
+ * @returns {Error} 404 - Book not found.
+ * @throws {Error} Throws a 500 error if there was an internal server error.
+*/
+app.get('/book/:isbn', async (req, res) =>
+{
+    const { isbn } = req.params;
+    try
+    {
+        const book = await db.Book.find({ isbn });
+        if (!book) return res.status(404).json({ message: 'Book not found' });
+        res.status(200).json({ message: "OK", data: book });
+    } catch (err)
+    {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 // =====================================================================
 //  SERVER LAUNCH
 // =====================================================================
