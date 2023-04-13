9ddbfcb21dadc27be0c3ee3fc66847a996b1bbc5
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-13 16:01:26 +0100

    feat(routes): add POST route

--- src/server.js
+++ src/server.js
@@ -11,11 +11,41 @@ const app = express()
     .use(express.json());
 
 // =====================================================================
-//  ROUTES
+//  COMMANDS
 // =====================================================================
 
-app.get("/", async (_, res) => {
-    res.json({ message: "It Works!" });
+// Reference: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+/**
+* @typedef {object} BookRequestBody
+* @property {string} isbn - The book's ISBN as ISBN-10 or ISBN-13.
+* @property {string} genre - The book's genre.
+* @property {string} author - The book's author.
+* @property {string} title - The book's title.
+*/
+
+/**
+ * Create a new books.
+ *
+ * @route POST /books
+ * @group Books
+ * @param {BookRequestBody.model} req.body.required - The book to create.
+ * @returns {Book.model} 201 - The created book.
+ * @returns {Error} 500 - Internal server error.
+ */
+app.post('/books', async (req, res) =>
+{
+    try
+    {
+        const result = Array.isArray(req.body)
+            ? await db.Book.insertMany(req.body)
+            : await db.Book.create(req.body);
+        res.status(201).json({ message: "OK", data: result });
+    } catch (err)
+    {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 // =====================================================================
@@ -23,6 +53,7 @@ app.get("/", async (_, res) => {
 // =====================================================================
 
 // If there's a problem with the DB connection, we don't want the Web API server to start.
-db.connect().then(() => {
+db.connect().then(() =>
+{
     app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}.`));
 });
