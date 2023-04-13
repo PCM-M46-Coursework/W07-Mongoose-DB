d6799037fbe714d8e7aad47f90b6f7eba7b87636
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-13 19:21:38 +0100

    refactor: apply lazy loading to server app

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
-const express = require("express");
-const db = require("./db/context");
-const booksRouter = require("./routes/books");
-
-// Fluent Configuration
-const app = express()
-    .disable('TRACE')
-    .use(express.json())
-    .use('/books', booksRouter);
-
-// If there's a problem with the DB connection, we don't want the Web API server to start.
-db.connect().then(() =>
-{
-    app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}.`));
-});
+require("./db/context")
+    .connect()
+    .then(() =>
+    {
+        const express = require("express");
+        express()
+            .disable('TRACE')
+            .use(express.json())
+            .use('/books', require("./routes/books"))
+            .listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}.`));
+    });
