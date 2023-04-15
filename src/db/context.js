034484e05acea6c8b720035225e3d770e6f152ea
Apache <ApacheTech@users.noreply.github.com>
2023-04-15 17:00:26 +0100

    refactor: inline connect() within db context

--- src/db/context.js
+++ src/db/context.js
@@ -1,21 +1,20 @@
 require("dotenv").config({ path: "../../config/dev.env" });
 const mongoose = require("mongoose");
 
-/**
- * Connect to the databse, using credentials, and connection strings within the environment files.
- */
-async function connect()
+module.exports =
 {
-    try
+    /**
+     * Connect to the databse, using credentials, and connection strings within the environment files.
+     */
+    connect = async function()
     {
-        await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
-    } catch (error)
-    {
-        console.log(error);
+        try
+        {
+            await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+        } catch (error)
+        {
+            console.log(error);
+        }
+        console.log("DB Connection established.");
     }
-    console.log("DB Connection established.");
-}
-
-module.exports = {
-    connect
 };
